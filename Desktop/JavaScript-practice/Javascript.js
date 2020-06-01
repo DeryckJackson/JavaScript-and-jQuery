ca5a4bf90b23281af1c60dd6d99aac8313907c2e
Ian Gregg <iangregg188@gmail.com>
2020-06-01 10:10:14 -0700

    practiced using javascript functions
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

---
+++ Desktop/JavaScript-practice/Javascript.js
@@ -0,0 +1,14 @@
+function SayName(name){
+  console.log("hi " + name)
+}
+
+const myName = "Ian"
+SayName(myName);
+SayName("Deryck");
+
+function mutipl(num1, num2){
+  console.log(num1 * num2)
+}
+const number1 = 2
+const number2 = 4
+mutipl(number1, number2);
